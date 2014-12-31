fe67a0ff2c8570d4c495351624b72b7dd355fcb7
Lee Campbell <leecam@chromium.org>
2014-12-30 22:22:33 -0800

    Add a basic service

--- src/org/leecam/basic_app/BasicAppActivity.java
+++ src/org/leecam/basic_app/BasicAppActivity.java
@@ -1,7 +1,15 @@
 package org.leecam.basic_app;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +17,8 @@ import android.widget.LinearLayout;
 
 public class BasicAppActivity extends Activity {
   public static final String TAG = "BasicAppActivity";
+  boolean mBound;
+  Messenger mService;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -21,17 +31,59 @@ public class BasicAppActivity extends Activity {
     setContentView(contentView);
 
     // Create a simple button
-    Button simpleButton = new Button(this);
-    simpleButton.setText("Simple Button");
-    simpleButton.setOnClickListener(new View.OnClickListener() {
+    Button startServiceButton = new Button(this);
+    startServiceButton.setText("Start Service");
+    final BasicAppActivity ctx = this;
+    startServiceButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        Log.d(TAG, "Button Clicked");
+        Log.d(TAG, "Starting Service");
+        bindService(new Intent(ctx, BasicAppService.class),
+                    mConnection,
+                    Context.BIND_AUTO_CREATE);
       }
     });
-
     // Add button to view
-    contentView.addView(simpleButton);
+    contentView.addView(startServiceButton);
+    contentView.addView(
+        createRemoteMessageButton("Service - Say Hello",
+                                  BasicAppService.MSG_SAY_HELLO));
   }
+
+  // Creates a button to send a message to the service
+  // This boiler plate code was stolen from rsesek!
+  private Button createRemoteMessageButton(final String label,
+                                           final int messageID) {
+    Button button = new Button(this);
+    button.setText(label);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (mBound) {
+          try {
+            mService.send(Message.obtain(null, messageID, 0, 0));
+          } catch (RemoteException ex) {
+            Log.e(TAG, ex.getMessage(), ex);
+          }
+        } else {
+          Log.e(TAG, "Need to start service");
+        }
+      }
+    });
+    return button;
+  }
+
+  private ServiceConnection mConnection = new ServiceConnection() {
+    @Override
+    public void onServiceConnected(ComponentName className, IBinder service) {
+      mService = new Messenger(service);
+      mBound = true;
+    }
+    @Override
+    public void onServiceDisconnected(ComponentName className) {
+      mService = null;
+      mBound = false;
+    }
+  };
 
 }
---
+++ src/org/leecam/basic_app/BasicAppService.java
@@ -0,0 +1,42 @@
+package org.leecam.basic_app;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+
+public class BasicAppService extends Service {
+
+  public static final String TAG = "BasicAppService";
+
+  static final int MSG_SAY_HELLO = 1;
+
+  @Override
+  public void onCreate() {
+    Log.i(TAG,"BasicAppService Created");
+  }
+
+  class MessageHandler extends Handler {
+    @Override
+    public void handleMessage(Message msg) {
+      switch (msg.what) {
+        case MSG_SAY_HELLO:
+          Log.i(TAG, "MSG_SAY_HELLO");
+          break;
+        default:
+          super.handleMessage(msg);
+      }
+    }
+  }
+
+  Messenger mMessenger = new Messenger(new MessageHandler());
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    Log.i(TAG,"BasicAppService Binding");
+    return mMessenger.getBinder();
+  }
+}
