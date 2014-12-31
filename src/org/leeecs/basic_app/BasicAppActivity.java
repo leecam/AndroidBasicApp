fbfdce3a26afbe95fe2e89ca1b1a67971a6400e9
Lee Campbell <leecam@chromium.org>
2014-12-30 20:58:32 -0800

    Added a simple button

--- src/org/leeecs/basic_app/BasicAppActivity.java
+++ src/org/leeecs/basic_app/BasicAppActivity.java
@@ -3,6 +3,9 @@ package org.leeecs.basic_app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class BasicAppActivity extends Activity {
   public static final String TAG = "BasicAppActivity";
@@ -11,6 +14,24 @@ public class BasicAppActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     Log.d(TAG, "Started");
+
+    // Set up a basic view
+    LinearLayout contentView = new LinearLayout(this);
+    contentView.setOrientation(LinearLayout.VERTICAL);
+    setContentView(contentView);
+
+    // Create a simple button
+    Button simpleButton = new Button(this);
+    simpleButton.setText("Simple Button");
+    simpleButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Log.d(TAG, "Button Clicked");
+      }
+    });
+
+    // Add button to view
+    contentView.addView(simpleButton);
   }
 
 }
